1cc2101c7499ccaeb43eaa2ee485b6257f3c202c
Qoutel Qoutel <nezzy124@outlook.com>
2022-01-31 15:22:02 +0200

    Add project files.

---
+++ WCFserviceClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WCFserviceClient.ServiceReference1;
+
+namespace WCFserviceClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorClient client = new CalculatorClient();
+
+            double value1 = 100.00D;
+            double value2 = 15.99D;
+            double result = client.Add(value1, value2);
+            Console.WriteLine("Operation Add({0};{1})\t Result = {2}", value1, value2, result);
+
+            value1 = 145.00D;
+            value2 = 76.54D;
+            result = client.Subtract(value1, value2);
+            Console.WriteLine("Operation Subtract({0};{1})\t Result = {2}", value1, value2, result);
+
+            value1 = 9.00D;
+            value2 = 81.25D;
+            result = client.Multiply(value1, value2);
+            Console.WriteLine("Operation Multiply({0};{1})\t Result = {2}", value1, value2, result);
+
+            value1 = 22.00D;
+            value2 = 7.00D;
+            result = client.Divide(value1, value2);
+            Console.WriteLine("Operation Divide({0};{1})\t Result = {2}", value1, value2, result);
+
+            Console.WriteLine("\nPress <Enter> to terminate the client.");
+            Console.ReadLine();
+            client.Close();
+        }
+    }
+}
---
+++ WCFserviceHost/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using WCFserviceLib;
+
+namespace WCFserviceHost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8733/WCFservice");
+
+            ServiceHost selfHost = new ServiceHost(typeof(CalculatorService), baseAddress);
+
+            try
+            {
+                selfHost.AddServiceEndpoint(typeof(ICalculator), new WSHttpBinding(), "CalculatorService");
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smb);
+                selfHost.Open();
+                Console.WriteLine("The service is ready.");
+                Console.WriteLine("Press <Enter> to terminate the service.");
+                Console.WriteLine();
+                Console.ReadLine();
+                selfHost.Close();
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine("An exception occurred: {0}", ce.Message);
+                selfHost.Abort();
+            }
+        }
+    }
+}
